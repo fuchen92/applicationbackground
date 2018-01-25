2622350d05318dab7b7dcb05133eb61830e43240
浮尘 <757213882@qq.com>
2018-01-25 15:42:42 +0800

    完成allot页面逻辑

--- js/allot.js
+++ js/allot.js
@@ -52,7 +52,7 @@ $(document).ready(function () {
         }
         return true;
     }
-    $("#noallot").on("click keyup",".addallot-form :input",function() {
+    $("#noallot, #changeDialog").on("click keyup",":input",function() {
         $(".form-tips").html("").removeAttr("style");
     });
 
@@ -149,7 +149,7 @@ $(document).ready(function () {
         $(".noallot-count").eq(1).text(noAllotCount);
         $(".already-allot").eq(0).text(alreadyAllotCount);
         $(".already-allot").eq(1).text(alreadyAllotCount);
-        noAllotCount - allotUsers.length < 0 && $("#noallotContainer").remove();
+        noAllotCount - allotUsers.length < 0 && $("#noallotWrapper").remove();
         console.log(noAllotCount)
         console.log("提交成功啦")
         noAllotContainer.find(":input[type=text]").val("");
@@ -162,8 +162,14 @@ $(document).ready(function () {
             var objValue = objArr[i];
             for (var j = 0; j < objArr.length; j++) {
                 if (objArr[j] == objValue && j != i) {
-                    elem.eq(j).focus();
-                    elem.eq(j).next().text(type + "重复，请检查").show();
+                    if (elem[0].id == "changeMobile" || elem[0].id == "changeEmail") {
+                        $(elem[0]).focus();
+                        $(elem[0]).next(".form-tips").text(type + "重复，请检查").show();
+                    } else {
+                        elem.eq(j).focus();
+                        elem.eq(j).next().text(type + "重复，请检查").show();
+                    }
+                    
                     catchErr = true;
                     return false;
                 }
@@ -178,35 +184,35 @@ $(document).ready(function () {
     function generateAlloted (objArr) {
         var str = "";
         for (var i = 0; i < objArr.length; i++) {
-            str += "<ul class='alloted'>" + 
+            str += "<ul class='alloted' data-industry='" + objArr[i].Industry + "'data-function='" + objArr[i].JobFunction + "'data-sex='" + objArr[i].Sex + "'>" + 
                         "<li class='alloted-item'>" +
-                            "<span class='allot-name'>" + objArr[i].NameCn +"</span>" +
+                            "<span class='allot-name allotVal'>" + objArr[i].NameCn +"</span>" +
                             "<b class='divide'></b>" + 
-                            "<span class='allot-nameEN'>" + objArr[i].NameEn + "</span>" +
+                            "<span class='allot-nameEN allotVal'>" + objArr[i].NameEn + "</span>" +
                         "</li>" +
                         "<li class='alloted-item'>" + 
-                            "<span class='allot-company'>" + objArr[i].CompanyCn + "</span>" + 
+                            "<span class='allot-company allotVal'>" + objArr[i].CompanyCn + "</span>" + 
                             "<b class='divide'></b>" + 
-                            "<span class='allot-companyEN'>" + objArr[i].CompanyEn + "</span>" + 
+                            "<span class='allot-companyEN allotVal'>" + objArr[i].CompanyEn + "</span>" + 
                         "</li>" +
                         "<li class='alloted-item'>" + 
-                            "<span class='allot-job'>" + objArr[i].JobTitleCn + "</span>" + 
+                            "<span class='allot-job allotVal'>" + objArr[i].JobTitleCn + "</span>" + 
                             "<b class='divide'></b>" + 
-                            "<span class='allot-jobEN'>" + objArr[i].JobTitleEn + "</span>" + 
+                            "<span class='allot-jobEN allotVal'>" + objArr[i].JobTitleEn + "</span>" + 
                         "</li>" +
                         "<li class='alloted-item'>" + 
-                            "<span class='allot-industry'>" + $(".allot-select").eq(0).children("option[value=" + objArr[i].Industry + "]").text() + "</span>" + 
+                            "<span class='allot-industry allotVal'>" + $(".allot-select").eq(0).children("option[value=" + objArr[i].Industry + "]").text() + "</span>" + 
                             "<b class='divide'></b>" + 
-                            "<span class='allot-function'>" + $(".allot-select").eq(1).children("option[value=" + objArr[i].JobFunction + "]").text() + "</span>" + 
+                            "<span class='allot-function allotVal'>" + $(".allot-select").eq(1).children("option[value=" + objArr[i].JobFunction + "]").text() + "</span>" + 
                             "<b class='divide'></b>" + 
-                            "<span class='allot-sex'>" + $(".allot-select").eq(2).children("option[value=" + objArr[i].Sex + "]").text() + "</span>" +
+                            "<span class='allot-sex allotVal'>" + $(".allot-select").eq(2).children("option[value=" + objArr[i].Sex + "]").text() + "</span>" +
                         "</li>" +
                         "<li class='alloted-item'>" + 
-                            "<span class='allot-mobile mobile'>" + objArr[i].Mobile + "</span>" + 
+                            "<span class='allot-mobile mobile allotVal'>" + objArr[i].Mobile + "</span>" + 
                             "<b class='divide'></b>" + 
-                            "<span class='allot-tel'>" + objArr[i].Tel + "</span>" + 
+                            "<span class='allot-tel tel allotVal'>" + objArr[i].Tel + "</span>" + 
                             "<b class='divide'></b>" + 
-                            "<span class='allot-email email'>" + objArr[i].Mail + "</span>" + 
+                            "<span class='allot-email email allotVal'>" + objArr[i].Mail + "</span>" + 
                             "<button class='changeAllotBtn rt'>修改</button>" +
                         "</li>" +
                     "</ul>"
@@ -218,31 +224,138 @@ $(document).ready(function () {
     var changeDialog = $("#changeDialog"),
         submitChangeAllot = $("#submitChangeAllot");
 
+    var allotIndex = 0;                 // 修改的是哪一个分配信息的下标
+
     var changeLis = changeDialog.find(".form-group");
     // 修改分配信息
     allotedContainer.on("click", ".changeAllotBtn", function () {
-        // var inputs = changeDialog.find(":input:not(button)");
-        var lis = $(this).closest(".alloted").children("li");
+        
+        var alloted = $(this).closest(".alloted");
+        var lis = alloted.children("li");
+
+        allotIndex = alloted.index(".alloted");
+        console.log(allotIndex)
+
         lis.each(function (index, item) {
             var item = $(item);
             var allotVals = item.children(".allotVal");
-            console.log(allotVals);
+            
             if (index < 3) {
                 allotVals.each(function (idx) {
                     changeLis.eq(index).find(":input").eq(idx).val(item.children(".allotVal").eq(idx).text())
                 })
+                return;
             }
-            if (index == 3) {
-                
-            }
+            
         });
+
+        $("#changeMobile").val(lis.eq(4).find(".mobile").text());
+        $("#changeTel").val(lis.eq(4).find(".tel").text());
+        $("#changeEmail").val(lis.eq(4).find(".email").text());
+
+        $("#changeIndustry option[value=" + alloted.attr("data-industry") + "]").prop("selected", true);
+        $("#changeFunction option[value=" + alloted.attr("data-function") + "]").prop("selected", true);
+        $("#changeSex option[value=" + alloted.attr("data-sex") + "]").prop("selected", true);
         
         changeDialog.css("display", "block");
     });
     $(".changeAllot-close").on("click", function () {
+        changeDialog.find(".form-tips").removeAttr("style");
+        changeDialog.find(":input:not(select)").val("");
+        changeDialog.find("select").val(0);
         changeDialog.removeAttr("style");
     });
-    submitChangeAllot.on("click", function () {
 
+    // 保存修改的分配信息
+    submitChangeAllot.on("click", function () {
+        var inputs = changeDialog.find(":input:not(button)"),
+            changedInfo = {};
+
+        var mobileArr = [],
+            emailArr = [];
+
+        var alloteds = allotedContainer.children(".alloted"),
+            alloted = alloteds.eq(allotIndex),
+            lis = alloted.children("li");
+
+        catchErr = false;
+
+        inputs.each(function () {
+            var currInput = $(this);
+            if (!validate(currInput)) {
+                catchErr = true;
+                currInput.focus();
+                return false;
+            }
+        });
+        if (catchErr) {
+            return false;
+        }
+
+        alloteds.each(function (index, item) {
+            var currItem = $(item);
+            if (index != allotIndex) {
+                mobileArr.push( $.trim( currItem.find(".mobile").text() ) );
+                emailArr.push( $.trim( currItem.find(".email").text() ) )
+            }
+        });
+        mobileArr.push( $.trim( $("#changeMobile").val() ) );
+        emailArr.push( $.trim( $("#changeEmail").val() ) );
+
+        valiRepeat(mobileArr, $("#changeMobile"), "手机号码");
+        if (catchErr) {
+            return false;
+        }
+        valiRepeat(emailArr, $("#changeEmail"), "邮箱地址");
+        if (catchErr) {
+            return false;
+        }
+
+        // $.ajax({
+        //     url: "OrderCreate",
+        //     type: "POST",
+        //     dataType: "json",
+        //     contentType: "application/json; charset=utf-8",
+        //     data: JSON.stringify({ "order": order }),
+        //     cache: false,
+        //     success: function (js) {
+        //         if (js.Code == 0) {
+        //             alert("提交成功")
+        //         } else {
+        //             alert("提交失败，请稍后再试")
+        //         }
+        //     },
+        //     error: function (error) {
+        //         console.log(error)
+        //     }
+        // });
+
+        lis.each(function (index, item) {
+            var item = $(item);
+            var allotVals = item.children(".allotVal");
+            
+            if (index < 3) {
+                allotVals.each(function (idx) {
+                    lis.eq(index).find(".allotVal").eq(idx).text( $.trim( changeLis.eq(index).find(":input").eq(idx).val() ) )
+                })
+                return;
+            }
+        });
+        lis.eq(3).find(".allot-industry").text($("#changeIndustry option:selected").text());
+        lis.eq(3).find(".allot-function").text($("#changeFunction option:selected").text());
+        lis.eq(3).find(".allot-sex").text($("#changeSex option:selected").text());
+
+        lis.eq(4).find(".mobile").text( $.trim($("#changeMobile").val()) )
+        lis.eq(4).find(".tel").text( $.trim($("#changeTel").val()) )
+        lis.eq(4).find(".email").text( $.trim($("#changeEmail").val()) )
+
+        alloted.attr("data-industry", $("#changeIndustry").val());
+        alloted.attr("data-function", $("#changeFunction").val());
+        alloted.attr("data-sex", $("#changeSex").val());
+
+        $(".changeAllot-close").trigger("click");
+
+        console.log("修改成功啦")
+        
     });
 });
